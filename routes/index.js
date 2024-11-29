2d852fb6ea9c1787aa57506717743537b425bb66
danya <danny.glazovs@gmail.com>
2024-11-29 18:56:39 +0300

    get the test application

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,71 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config()
+// const puppeteer = require("puppeteer");
+// require("dotenv").config()
+// exports.index = async function (req, res) {
+//     const cookies = await fetchCookies();
+//
+//     if (cookies) {
+//         req.session.data = cookies[0]
+//
+//     }
+//
+//     return res.render('index');
+// };
+//
+// async function fetchCookies() {
+//     const browser = await puppeteer.launch({
+//         headless: true,
+//         defaultViewport: null,
+//         executablePath:
+//         process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
+        // executablePath: '/usr/bin/google-chrome',
+        // args: [
+        //     '--no-sandbox',
+        //     '--disable-setuid-sandbox',
+        //     '--disable-dev-shm-usage',
+        //     '--disable-accelerated-2d-canvas',
+        //     '--disable-gpu',
+        //     '--no-zygote',
+        //     // '--single-process'
+        // ]
+//     });
+//     const page = await browser.newPage();
+//     console.log("Opening browser...");
+//     console.log("Using executable path:", browser.session);
+//
+//     try {
+//         console.log("Open Dnevnik...");
+//         await page.goto("https://www.google.com/", {
+//             // waitUntil: "networkidle2",
+//         });
+//
+//         console.log("Wait for authorization...");
+//
+//         // await page.waitForFunction(
+//         //     () => document.cookie.includes("X-JWT-Token"),
+//         // );
+//
+//         const cookies = await page.cookies();
+//         console.log("Success for take cookies");
+//         console.log(cookies);
+//
+//         await browser.close();
+//
+//         return cookies;
+//     } catch (error) {
+//         console.error("Error:", error.message);
+//         await browser.close();
+//     }
+// }
+
 exports.index = async function (req, res) {
-    const cookies = await fetchCookies();
+    const puppeteer = require('puppeteer');
+    require('dotenv').config();
 
-    if (cookies) {
-        req.session.data = cookies[0]
-
-    }
-
-    return res.render('index');
-};
-
-async function fetchCookies() {
     const browser = await puppeteer.launch({
         headless: true,
         defaultViewport: null,
         executablePath:
-        process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
-        // executablePath: '/usr/bin/google-chrome',
+            process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
@@ -25,34 +73,30 @@ async function fetchCookies() {
             '--disable-accelerated-2d-canvas',
             '--disable-gpu',
             '--no-zygote',
-            // '--single-process'
         ]
     });
     const page = await browser.newPage();
-    console.log("Opening browser...");
-    console.log("Using executable path:", browser.session);
 
-    try {
-        console.log("Open Dnevnik...");
-        await page.goto("https://www.google.com/", {
-            // waitUntil: "networkidle2",
-        });
+// Navigate the page to a URL.
+    await page.goto('https://developer.chrome.com/');
 
-        console.log("Wait for authorization...");
+// Set screen size.
+    await page.setViewport({width: 1080, height: 1024});
 
-        // await page.waitForFunction(
-        //     () => document.cookie.includes("X-JWT-Token"),
-        // );
+// Type into search box.
+    await page.locator('.devsite-search-field').fill('automate beyond recorder');
 
-        const cookies = await page.cookies();
-        console.log("Success for take cookies");
-        console.log(cookies);
+// Wait and click on first result.
+    await page.locator('.devsite-result-item-link').click();
 
-        await browser.close();
+// Locate the full title with a unique string.
+    const textSelector = await page
+        .locator('text/Customize and automate')
+        .waitHandle();
+    const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
-        return cookies;
-    } catch (error) {
-        console.error("Error:", error.message);
-        await browser.close();
-    }
-}
+// Print the full title.
+    console.log('The title of this blog post is "%s".', fullTitle);
+    await browser.close();
+    return res.send(fullTitle);
+};
